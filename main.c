d5cfda0a242147f6bb78950f641dcf278cbfeea2
Jevago <jonathanvazquezglz@gmail.com>
2014-08-28 11:03:26 -0500

    SE AGREGARON LIBRERIAS NECESARIAS PARA EL PROGRAMA Y UNA EXPLICACIÓN MÁS DETALLA DE LA FUNCION PRINCIPAL

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 int main( int argc, char **argv ) 
 {
 	/**
-	 *	FUNCION PRINCIPAL DEL PROGRAMA.
+	 *	\brief FUNCION PRINCIPAL DEL PROGRAMA.
 	 *
-	 *
-	 *
+	 *	LA FUNCION VERIFICA QUE EL NUMERO TOTAL DE PARAMETROS SEA IGUAL A 1, SINO COMPLE CON ESTA CONDICIÓN
+	 *	LA FUNCION MUESTRA UN MENSAJE DE ERROR Y TERMINA CON UN CÓDIGO DE ERROR.
 	 *
 	 */
 
